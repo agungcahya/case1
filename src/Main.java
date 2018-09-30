6a3b6665d522da8c8b71b4dca2f1268df757da94
agungcahya <agungcahyailhami@gmail.com>
2018-09-30 22:28:38 +0700

    input and output

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main (String[] args){
+        Double harga;
+        String kode;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Amount: ");
+        harga = s.nextDouble();
+        s.nextLine();
+        System.out.print("Kode Promo: ");
+        kode = s.nextLine();
+        Hitung h =new Hitung(harga, kode);
+        System.out.println();
+        System.out.println("Diskon: "+h.diskon+"%");
+        System.out.println("Amount yang harus dibayar: "+h.bayar());
     }
 }
